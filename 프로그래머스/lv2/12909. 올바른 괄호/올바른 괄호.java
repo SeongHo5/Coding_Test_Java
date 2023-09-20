4d40d83219d3ed85f21f631efff0f43eef925be2
SeongHo5 <newfuring1123@gmail.com>
2023-09-20 18:28:06 +0900

    [level 2] Title: 올바른 괄호, Time: 8.23 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,24 @@
 import java.util.Stack;
 
 class Solution {
-    boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    stack.pop();
-                } else {
-                    return false;
-                }
+   boolean solution(String s) {
+        boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
             }
         }
-        return stack.isEmpty();
+        if(count == 0){
+            answer = true;
+        }
+
+        return answer;
     }
 }
