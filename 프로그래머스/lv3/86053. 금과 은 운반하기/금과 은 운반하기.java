cbfffc905c4292fcf7b54b2137d8663a3449e14c
SeongHo5 <newfuring1123@gmail.com>
2023-09-21 17:16:20 +0900

    [level 3] Title: 금과 은 운반하기, Time: 0.09 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Solution {
+	public boolean isPossible(long time, int a, int b, int[] g, int[] s, int[] w, int[] t) {
+	    int n = g.length;
+	    long total = 0L;
+	    long totalG = 0L;
+	    long totalS = 0L;
+
+	    for (int i = 0; i < n; i++) {
+	        long cnt = time / (2L * t[i]);
+	        if (time % (2L * t[i]) >= t[i]) cnt++;
+
+	        // 해당 시간에 옮길 수 있는 무게
+	        long tmp = Math.min(cnt * w[i], g[i] + s[i]);
+	        // 각 도시의 총합 최대 무게 누적
+	        total += tmp;
+	        totalG += Math.min(tmp, g[i]);
+	        totalS += Math.min(tmp, s[i]);
+	    }
+
+	    return total >= a + b && totalG >= a && totalS >= b;
+	}
+
+	public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
+	    long hi = 400000000000000L;
+	    long lo = 0;
+
+	    // 이분 탐색
+	    while (lo + 1 < hi) {
+	        long mid = (lo + hi) / 2;
+
+	        if (isPossible(mid, a, b, g, s, w, t)) hi = mid;
+	        else lo = mid;
+	    }
+	    return hi;
+	}
+}
