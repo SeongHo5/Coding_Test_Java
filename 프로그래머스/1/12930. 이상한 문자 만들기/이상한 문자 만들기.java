cf9eb3f4b3ea46fba797b9916f0ba0bcce0b9d58
SeongHo5 <newfuring1123@gmail.com>
2024-02-19 17:24:37 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                sb.append(c);
+                index = 0;
+            } else {
+                if (index % 2 == 0) {
+                    sb.append(Character.toUpperCase(c)); 
+                } else {
+                    sb.append(Character.toLowerCase(c));
+                }
+                index++;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
